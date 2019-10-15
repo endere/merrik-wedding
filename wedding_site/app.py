0ec3c70ac5c17610fc87ed23416c3caf92d48dec
Merry Cimakasky <mkcimakasky@gmail.com>
2019-10-14 20:51:42 -0700

    paws

--- wedding_site/app.py
+++ wedding_site/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder='static/templates')
 
 @app.route('/')
 def hello_world(): 
-  return render_template('base.html')
+  return render_template('base.html', message='Hello Wedding!')
 
 if __name__ == '__main__': 
   app.run(host='0.0.0.0', debug=True)  
