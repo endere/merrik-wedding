1ceee2655c43fc1fbb51fed98ed2adb35d0f5a27
Erik Enderlein <erik@usermind.com>
2019-10-18 15:50:26 -0700

    procfile

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""
+merrick wedding
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='merrick_weeding',
+    version='0.0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'sqlalchemy',
+        'docker',
+        'flask-sqlalchemy',
+        'psycopg2',
+        'passlib',
+        'sqlalchemy_utils',
+        'docker-compose'
+    ],
+    test_require=[
+        'pytest',
+        'pytest-cov',
+    ]
+)
--- wedding_site/app.py
+++ wedding_site/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='static/templates')
+print('hello world!!!')
+
 
 @app.route('/')
 def hello_world(): 
