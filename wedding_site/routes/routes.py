32fa825ba602d82baa04999b4fe7dac2d7a7fe9a
Erik Enderlein <erik@usermind.com>
2020-03-08 14:38:27 -0700

    test push

--- wedding_site/routes/routes.py
+++ wedding_site/routes/routes.py
@@ -4,4 +4,4 @@ routes = Blueprint('routes', __name__, template_folder='static/templates')
 
 @routes.route('/')
 def hello_world(): 
-  return render_template('base.html', message='Hello Wedding!')
+  return render_template('base.html', message='Hello Wedding test!')
