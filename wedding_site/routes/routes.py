c3153d05e6eb845630c2434c2a0a88eb9e3b00e0
Merry Cimakasky <mkcimakasky@gmail.com>
2020-03-11 15:59:56 -0700

    reorg, some styling

--- wedding_site/routes/routes.py
+++ wedding_site/routes/routes.py
@@ -4,7 +4,7 @@ routes = Blueprint('routes', __name__, template_folder='static/templates')
 
 @routes.route('/')
 def hello_world(): 
-    return render_template('home.html')
+    return render_template('index.html')
 
 
 
