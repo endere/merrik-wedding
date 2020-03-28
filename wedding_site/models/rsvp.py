d39ff4c21e2cae6704de1d28cff96ca501dd5fce
Erik Enderlein <erik@usermind.com>
2020-03-27 20:22:36 -0700

    more css omggg

--- wedding_site/models/rsvp.py
+++ wedding_site/models/rsvp.py
@@ -3,12 +3,14 @@ from wedding_site import db
 class RSVP(db.Model): 
     name = db.Column(db.String())
     email = db.Column(db.String(), primary_key=True)
-    meal_preference = db.Column(db.String())
+    dietary_restrictions = db.Column(db.String())
+    number_of_guests = db.Column(db.Integer())
 
-    def __init__(self, name, email, meal_preference): 
+    def __init__(self, name, email, number_of_guests, dietary_restrictions): 
       self.name = name 
       self.email = email
-      self.meal_preference = meal_preference
+      self.dietary_restrictions = dietary_restrictions
+      self.number_of_guests = number_of_guests
 
     def __repr__(self):
       return '<RSVP {}>'.format(self.body)
